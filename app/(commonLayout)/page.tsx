6846485edc58a848a8888df41dce17e9e09a1194
ronSiuuuu <mehedihasanakash1717@gmail.com>
2026-02-13 22:48:28 +0600

    feat(auth): implemented google login with better auth

--- app/(commonLayout)/page.tsx
+++ app/(commonLayout)/page.tsx
@@ -5,12 +5,11 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Star } from "lucide-react";
 import { featuredTutors } from "@/src/lib/mock-data";
-import { authClient } from "@/src/lib/auth-client";
+
 
 
 export default async function Home() {
-  const session = await authClient.getSession();
-  console.log(session);
+ 
   return (
     <div className="flex flex-col min-h-screen">
       {/* Hero */}
