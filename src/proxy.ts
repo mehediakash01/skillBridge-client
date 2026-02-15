38d91558ff65292f0e724553da87064f5c282ff6
ronSiuuuu <mehedihasanakash1717@gmail.com>
2026-02-15 16:17:54 +0600

    feat(protected routes): added proxy to protect routes and unauthorized access

---
+++ src/proxy.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import { userService } from "./services/user.service";
+import { Roles } from "./constatns/role";
+
+export async function proxy(request:NextRequest){
+    const pathName = request.nextUrl.pathname;
+    let isAuthenticated = false;
+    let isAdmin = false;
+    
+    const {data} = await userService.getSession();
+    if (data){
+        isAuthenticated=true;
+        isAdmin = data.user.role===Roles.admin
+    }
+
+    if (!isAuthenticated){
+        return NextResponse.redirect(new URL("/login",request.url));
+    }
+
+    if (isAdmin){
+        return NextResponse.redirect(new URL("/admin-dashboard",request.url))
+    }
+    if (!isAdmin && pathName.startsWith("/admin-dashboard")){
+        return NextResponse.redirect(new URL("/dashboard",request.url))
+    }
+    return NextResponse.next();
+
+}
+export const config ={
+    matcher:["/dashboard","/dashboard/:path*","/admin-dashboard","/admin-dashboard/:path*"]
+}
